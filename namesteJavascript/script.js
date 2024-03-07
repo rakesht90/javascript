2bfbc0f8174bebbbafdd94650111e3b34fd87e4f
Rakesh <rakesh.jmi90@gmail.com>
2024-03-07 21:59:31 +0530

    Update script.js

--- namesteJavascript/script.js
+++ namesteJavascript/script.js
@@ -1,7 +1,17 @@
-var x = 1;
-a();
-console.log(x);
-function a() {
-  var x = 10;
-  console.log(x);
+function reducer(nums, fn, init) {
+  let result = init; // Initialize result with init value
+
+  for (let i = 0; i < nums.length; i++) {
+    result = fn(result, nums[i]);
+  }
+
+  return result;
 }
+
+// Example usage:
+const nums = [1, 2, 3, 4];
+const fn = function sum(accum, curr) {
+  return accum + curr;
+};
+const init = 0;
+console.log(reducer(nums, fn, init)); // Output: 10
